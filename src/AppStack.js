a88e06ddb8039b0c556fd962146aa21030d29542
khj2195 <khj2195@snu.ac.kr>
2021-03-15 21:01:54 +0900

    first commit

---
+++ src/AppStack.js
@@ -0,0 +1,28 @@
+import 'react-native-gesture-handler';
+import React, {Component} from 'react';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import ListSearchScreen from './screens/ListSearchScreen';
+
+const Stack = createStackNavigator();
+const AppStack = () => {
+    return(
+        <Stack.Navigator>
+            <Stack.Screen 
+            name="Home" 
+            component={HomeScreen}
+            options = {{
+                title: 'SoTong',
+            }}
+            />
+            <Stack.Screen 
+            name="ListSearch" 
+            component={ListSearchScreen}
+            options = {{title: '세부검색'}}
+            />
+        </Stack.Navigator>            
+        )
+}
+
+export default AppStack;
